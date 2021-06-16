22100467f3aeb6f9aaf669546f739af2feca61c2
ls <>
2021-06-16 21:53:57 +0800

    悦悦的C++

---
+++ yueyue/cmake-build-debug/CMakeFiles/yueyue.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yueyue.dir/main.cpp.obj"
+  "yueyue.exe"
+  "yueyue.exe.manifest"
+  "yueyue.lib"
+  "yueyue.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yueyue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
